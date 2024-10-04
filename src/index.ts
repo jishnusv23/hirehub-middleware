ca164e8bfd9230d6d615d5f30d5615cb66dc5224
jishnuvadakara <jishnuv750@gmail.com>
2024-10-04 11:19:08 +0530

    fix:added that modifythings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './utils/generateAccessToken'
 export * from './Middleware/jwtMiddleware'
 export * from './Middleware/roleMiddleware'
+export * from './Middleware/errorResponse'
+export * from './Middleware/errorhandler'
